876a7a0b80faa0c13fb485b20e32bb8e368667d6
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2025-03-24 16:41:25 -0300

    swagger

---
+++ src/main/java/com/acme/bytecard/infra/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.acme.bytecard.infra;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@OpenAPIDefinition(info = @Info(title = "API de Cartões", version = "v1"),
+        security = @SecurityRequirement(name = "bearerAuth"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
+public class SwaggerConfig {
+}
--- src/main/java/com/acme/bytecard/infra/security/SecurityConfigurations.java
+++ src/main/java/com/acme/bytecard/infra/security/SecurityConfigurations.java
@@ -27,6 +27,7 @@ public class SecurityConfigurations {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
 //                .requestMatchers(HttpMethod.GET, "/hello").permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
